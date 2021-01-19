fb4a6c227fa898ca6fe8d3716756846130cabe9a
sudeen <ranjitkarsudeen14@gmail.com>
2021-01-19 16:09:13 +1100

    configuring spotify api to get the artist and tracks

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,38 @@ import React, {Component} from 'react';
 import './App.css';
 import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com'
+
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            query: ''
+            query: '',
+            artist: null,
+            tracks: []
         }
     }
 
     search() {
         console.log('this.state', this.state);
-        const BASE_URL = 'https://api.spotify.com/v1/search?';
-      // type=artist" -H "Authorization: Bearer BQDHmudbfPp4He7eUll2RxbVKaIzxGAkx6JI5ZaJdTY2Z1VbJtbGCZcoJ_A2shqfx3CIISiKRxybLDVlwDWMDArgC8qElIS6lHGn9jRuRT9Ljd7pwQGTw8fAhH3T4UiRTzX4vW5STO6UTvExF1BEHl--AIBR_olCu9sTGv-xF0fQr0pQngGMcVSW2q0Geh-bpc0KWCUqY1Lp3aQw6yBJYjnaR8fKEbfk6WjebkUtJS8Hp1ifhNK2ss5wyL8xjQj48RdKgYSTtThyh3cIpgaWfpFWCUJ5GQY7YmABu2Z3o4fWQGQRT0pY39-2YXeDiQ
-        const FETCH_URL = `"${BASE_URL}q=${this.state.query}&type=artist&limit=1" -H "Authorization: Bearer BQDHmudbfPp4He7eUll2RxbVKaIzxGAkx6JI5ZaJdTY2Z1VbJtbGCZcoJ_A2shqfx3CIISiKRxybLDVlwDWMDArgC8qElIS6lHGn9jRuRT9Ljd7pwQGTw8fAhH3T4UiRTzX4vW5STO6UTvExF1BEHl--AIBR_olCu9sTGv-xF0fQr0pQngGMcVSW2q0Geh-bpc0KWCUqY1Lp3aQw6yBJYjnaR8fKEbfk6WjebkUtJS8Hp1ifhNK2ss5wyL8xjQj48RdKgYSTtThyh3cIpgaWfpFWCUJ5GQY7YmABu2Z3o4fWQGQRT0pY39-2YXeDiQ"  `;
-        console.log('FETCH_URL', FETCH_URL);
-        fetch(FETCH_URL, {
-            method: 'GET' ,
-            headers : {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'grant_type':'client_credentials'
+        fetch(`${API_ADDRESS}/artist/${this.state.query}`)
+        .then(response => response.json())
+        .then(json => {
+            if (json.artists.total > 0){
+                const artist =json.artists.items[0];
+                this.setState({artist});
+
+                fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                .then(response => response.json())
+                .then(json => this.setState({tracks: json.tracks}))
+                .catch(error => alert (error.message));
             }
         })
-            .then(response => response.json())
-            .then(json => console.log('json', json));
+        .catch(error => alert (error.message));
     }
 
     render() {
+        console.log("this.state", this.state);
         return (
             <div className="App">
                 <div className="App-title">Music Master</div>
